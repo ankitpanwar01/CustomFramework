15902ff79b203e1a338203fe62a95a7137f9d93b
Ankit Panwar <ankit.panwarmca@gmail.com>
2023-08-08 18:04:03 +0530

    version 0.0.2

---
+++ Sources/CustomFramework.xcframework/ios-arm64/CustomFramework.framework/Headers/CustomFramework.h
@@ -0,0 +1,18 @@
+//
+//  CustomFramework.h
+//  CustomFramework
+//
+//  Created by Ankit Panwar on 08/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomFramework.
+FOUNDATION_EXPORT double CustomFrameworkVersionNumber;
+
+//! Project version string for CustomFramework.
+FOUNDATION_EXPORT const unsigned char CustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomFramework/PublicHeader.h>
+
+
